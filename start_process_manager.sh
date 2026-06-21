16c3ef9df593ee15c4cf7547d1745d4a78ff9b60
Park Gwangsoo <pks87@nate.com>
2026-06-21 15:11:54 +0900

    load GSCAM_CONFIG from .bashrc in process manager startup script

--- start_process_manager.sh
+++ start_process_manager.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
 source ~/autoRCcar/ros2/install/setup.bash
+eval "$(grep '^export GSCAM' ~/.bashrc)"
 sleep 3
 exec ros2 run autorccar_utils process_manager_node
